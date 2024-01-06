2ef3a37918add1129f070f0f576974681643fd53
ammon134 <ammon134@gmail.com>
2024-01-06 16:08:16 +0700

    ft: add session support

--- src/app.js
+++ src/app.js
@@ -4,13 +4,20 @@ const path = require('path');
 const ejs = require('ejs');
 const debug = require('debug')('app');
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const { connect } = require('./config/dbConfig');
+
 // Import routes
 const { indexRouter } = require('./routes/index');
 const { ingredientsRouter } = require('./routes/ingredients');
 const { apiRouter } = require('./routes/api');
 const { usersRouter } = require('./routes/users');
+const passport = require('passport');
 
 const app = express();
+
+// Middlewares
 app.use(express.json());
 
 app.use(morgan('tiny'));
@@ -18,6 +25,23 @@ app.use((req, res, next) => {
   debug('Received request:', req.method, req.path, req.body);
   next();
 });
+
+const sess = {
+  secret: 'very secret recipe',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false },
+  store: MongoStore.create({
+    client: connect().then((m) => m.connection.getClient()),
+  }),
+};
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1); // trust first proxy
+  sess.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sess));
+app.use(passport.session());
 
 app.set('views', path.join(__dirname, 'views/layout'));
 app.set('view engine', 'ejs');
